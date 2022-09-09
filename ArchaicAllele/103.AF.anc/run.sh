5bf3a102fd17d15d0ad69e14ce435bee10fa9040
Zhang Rui <40763366+Zhang-Rui-2018@users.noreply.github.com>
2022-09-09 16:42:26 +0800

    Delete run.sh

--- ArchaicAllele/103.AF.anc/run.sh
+++
@@ -1,6 +0,0 @@
-zcat ../../examples/YRI.chr22.vcf.gz | ../000.scripts/getAF | gzip > YRI.af.txt.gz
-
-#you need to replace the path to homo_sapiens_ancestor_GRCh37_e71
-path=/picb/humpopg-bigdata2/zhangrui/AS2_refData/ 
-../000.scripts/annoAnc YRI.af.txt.gz ${path}/human_ancestor/homo_sapiens_ancestor_GRCh37_e71/homo_sapiens_ancestor YRI.anc.txt
-gzip YRI.anc.txt
